4786165fffa59c60cb014dfb2b7b0631416f6264
g0r0kh <gorokhovivan09@gmail.com>
2024-08-24 20:20:00 +0300

    Add statistics info, merge with main

--- base.py
+++ base.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
+# import itertools
 
-# import xlsx
+# xlsx source import
 data = pd.read_excel('source/task_sample.xlsx',
                      sheet_name='Лист1')
 # transform dtype
@@ -10,45 +11,29 @@ data[['time_index']] = data[['time_index']].astype(str)
 data['iter_str'] = data['time_index'].str[:4].astype(str) + data['task_mark'].astype(str)
 # rename columns
 data = data.rename(columns={'values':'vals'})
-
 sort_list = sorted(set(data['iter_str']))
+
+# list catch outliers
+occup = []
 
 for i in sort_list:
     a = data[data['iter_str'] == i]
-    # a = a.reset_index()
-# outlier prob
-    data['outlier_bottom'] = ((np.percentile(np.array([a.vals]), 25) -
-                              (np.percentile(np.array([a.vals]), 75)-np.percentile(np.array([a.vals]), 25))*1.5)).astype(float)
-# outlier expl
-# data['outlier_bottom_expl'] = ((np.percentile(np.array([data.vals]), 25) -
-#                           (np.percentile(np.array([data.vals]), 75)-np.percentile(np.array([data.vals]), 25))*3)).astype(float)
+    a = a.reset_index()
+    # symmetric distribution
+    out_bottom = (np.percentile(np.array([a.vals]), 25) -
+                  (np.percentile(np.array([a.vals]), 75) - np.percentile(np.array([a.vals]), 25)) * 1.5).astype(int)
+    # .astype(str))
+    # negatively skewed
+    if out_bottom <= np.min(np.array([a.vals])):
+        # try Median - (Q3-Q2)*1.5
+        out_bottom_n = (np.percentile(np.array([a.vals]), 25) -
+                        (np.percentile(np.array([a.vals]), 75) - np.median(np.array([a.vals]))) * 1.5).astype(float)
+        occup.append(out_bottom_n)
+    else:
+        occup.append(out_bottom)
+# convert statistic iter list 2 Dataframe
+data_distr = pd.DataFrame([list(x) for x in zip(sort_list, occup)])
+# merge main data with statistics
+data.merge(data_distr, left_on='iter_str', right_on=0)
 
 
-# find perc
-
-# a = np.array([data.vals])
-# p = np.percentile(a, 50)
-# b = np.array(arr)
-
-# import numpy as np
-# a = np.array([1,2,3,4,5])
-# p = np.percentile(a, 50)
-
-# sort_list = sorted(set(data['period']))
-#
-# plt.ion()
-#
-# for i in sort_list:
-#     a = data[data['period'] == i]
-#     a = a.reset_index()
-#     # plt.clf()
-#     plt.grid()
-#
-#
-#     occ_list = sorted(set(a['Occupation']))
-#
-#     occup = []
-#
-#     for j in occ_list:
-#         b = a[a['Occupation'] == j]
-#         occup.append(b.RUR_net_000)
